1c6dd396e62df2a539ab240f8c1ed71cc1e12296
sudharsanmaran <smsudharsanmaran@gmail.com>
2024-05-20 17:57:33 +0530

    Refactor validate_input function to include detailed input parameter descriptions

--- source/main.py
+++ source/main.py
@@ -111,9 +111,32 @@ def validate_input(
     bb_file_number,
     bb_band_sd,
 ):
-    # Validate the input parameters
-    # todo
-    # 1. extract the inputs, even for multiple strategies go with squential manner
+    """
+    1. Instument should be a string
+    2. Strategy ID should be a string, should contain the strategy IDs separated by a comma and multiple conditions separated by a pipe
+    3. Start date should be a string in the format "dd/mm/yyyy hh:mm:ss"
+    4. End date should be a string in the format "dd/mm/yyyy hh:mm:ss"
+    5. Fractal file number should be a string
+    6. BB file number should be a string
+    7. BB band standard deviation should be one of the following: 2.0, 2.25, 2.5, 2.75, 3.0
+    8. Trail BB band standard deviation should be one of the following: 2.0, 2.25, 2.5, 2.75, 3.0
+    9. BB band column should be one of the following: "mean", "upper", "lower"
+    10. Trail BB band column should be one of the following: "mean", "upper", "lower"
+    11. Trade start time should be a string in the format "hh:mm:ss"
+    12. Trade end time should be a string in the format "hh:mm:ss"
+    13. Check fractal should be a boolean
+    14. Check BB band should be a boolean
+    15. Check trail BB band should be a boolean
+    16. Trail BB band direction should be one of the following: "higher", "lower"
+    17. Trade type should be one of the following: "Intraday", "Positional"
+    18. Allowed direction should be one of the following: "long", "short", "all"
+    19. Fractal exit count should be an integer or "all"
+    20. Long-short and entry-exit signals should be a string, should contain the signals separated by a comma and multiple conditions separated by a pipe
+    21. Portfolio IDs should be a string, should contain the portfolio IDs separated by a comma
+    22. count of strategy IDs should be equal to the count of portfolio IDs, irrespective of the number of conditions
+    23. long entry singals should added in short exit signals
+    24. short entry signals should added in long exit signals
+    """
     pass
 
 
@@ -497,7 +520,7 @@ def main():
     trail_bb_band_sd = 2.0  # standard deviations (2.0, 2.25, 2.5, 2.75, 3.0)
     bb_band_column = "mean"  # (mean, upper, lower)
     trail_bb_band_column = "mean"
-    trade_start_time = "09:15:00"
+    trade_start_time = "13:15:00"
     trade_end_time = "15:20:00"
     check_fractal = True
     check_bb_band = False
@@ -514,7 +537,7 @@ def main():
         return [signal.strip() for signal in signals]
 
     strategy_ids = [parse_signals(id.split(",")) for id in strategy_ids.split("|")]
-    # Create the strategy_signal_map for each strategy
+
     conditions = {
         "entry": {
             MarketDirection.LONG: [
